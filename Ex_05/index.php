c6285642ff64d039bbf9836c0b7cd366cdfb7726
alexb <acabrerag@campus.eug.es>
2022-09-21 13:47:52 +0200

    Primer commit

--- Ex_05/index.php
+++ Ex_05/index.php
@@ -1,10 +1,19 @@
 <?php
-$n=8;
+
+for($i=0;$i<=10;$i++){
+    for ($j=1; $j <=$i; $j++) {
+        echo '*';
+    }
+    echo'</br>';
+}
+
+$n=10;
 for($i=$n;$i>=1;$i--){
     for ($s = 1; $s <= $n; $s++) {// Imprime el número de espacios
+
         //echo '&nbsp;'; //para desplazar la pirámide
     }
-    for ($j=1; $j <=2*$i-1 ; $j++) {
+    for ($j=1; $j <=$i; $j++) {
         echo '*';
     }
     echo '<br>';
@@ -12,19 +21,5 @@ for($i=$n;$i>=1;$i--){
 echo '<hr>';
 
 
-//Tamaño de la línea más larga, la de en medio de ambos triángulos
-$num = 8;
-//bucle para el triángulo superior
-for($n=1; $n <= $num; $n++){
-    //echo '&nbsp','</br>';
-    //echo str_pad('', $n, '*') . '<br />';
-    echo '*';
-
-}
-
-//bucle para el triángulo invertido
-for($n=$num - 1; $n >= 1; $n--){
-    echo str_pad('', $n, '*') . '<br />';
-}
 ?>
 
